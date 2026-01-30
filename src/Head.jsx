e7dd886fc08790bf9f5f22e185ae63f7e9e6716a
DaK-code <dariusgavi124@gmail.com>
2026-01-30 12:59:23 +0000

    add some style to header

--- src/Head.jsx
+++ src/Head.jsx
@@ -1,10 +1,10 @@
 const Head = () => {
   return (
     <div className="header">
-      <h1>-- FAST REACT PIZZA CO. --</h1>
+      <h1>FAST REACT PIZZA CO.</h1>
 
       <span>OUR MENU</span>
-      <p style={{width :" 410px", textAlign :"center"}}>
+      <p style={{width :" 410px", textAlign :"center", paddingTop: "10px"}}>
         Authentic Italien cusine. 6 creative dishes to choose from. All From our
         stone oven, all organic, all delicious.
       </p>
